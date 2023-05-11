e35091f1112011f96fad87c3fbc66f5ff83bb0ef
Miroslav Sotirov <miroslavsotirov@gmail.com>
2023-05-11 11:32:46 +0300

    sdfsdg

--- chat.php
+++ chat.php
@@ -1,16 +1,30 @@
 <?php 
   include_once 'db.php';
-
   $sql = mysqli_query($con, "SELECT * FROM post ORDER BY id DESC");
 
-  while ($rows = mysqli_fetch_array($sql)){
+  $chatLogs = array();
 
+  while ($rows = mysqli_fetch_array($sql)){
     $message = $rows['message'];
     $name = $rows['user_name'];
     $user_image = $rows['user_image'];
+
+    $chatLogs[] = array(
+      'name' => $name,
+      'user_image' => $user_image,
+      'message' => $message
+    );
+  }
 ?>
-  <div class="chat users">
-    <div class="user-photo"><img src="images/<?php echo $user_image; ?>" alt=""></div>
-    <p class="chat-message"><?php echo '<span style="color:#9a5803; font-size:14px;">'.$name.'</span><br>'.$message; ?></p>
-  </div>
-<?php } ?>
+
+<div class="chatlogs">
+  <?php foreach (array_reverse($chatLogs) as $log) { ?>
+    <div class="chat users">
+      <div class="user-photo"><img src="image/<?php echo $log['user_image']; ?>" alt=""></div>
+      <p class="chat-message">
+        <span style="color:#9a5803; font-size:14px;"><?php echo $log['name']; ?></span><br>
+        <?php echo $log['message']; ?>
+      </p>
+    </div>
+  <?php } ?>
+</div>
